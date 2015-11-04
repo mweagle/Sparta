d1d99c85955ece150ccdc1cf063cc2cd436e178f
Matt Weagle <mweagle@gmail.com>
2015-11-04 08:46:15 -0800

    Add SNS permission & correct go vet bug

--- describe_test.go
+++ describe_test.go
@@ -43,17 +43,29 @@ func mockLambda2(event *sparta.LambdaEvent, context *sparta.LambdaContext, w *ht
 func sampleData() []*sparta.LambdaAWSInfo {
 	var lambdaFunctions []*sparta.LambdaAWSInfo
 	lambdaFn := sparta.NewLambda(LAMBDA_EXECUTE_ARN, mockLambda1, nil)
-	lambdaFn.Permissions = append(lambdaFn.Permissions, &lambda.AddPermissionInput{
-		Action:      aws.String("lambda:InvokeFunction"),
-		Principal:   aws.String("s3.amazonaws.com"),
-		StatementId: aws.String("Woor"),
-		SourceArn:   aws.String("arn:aws:s3:::myS3Bucket"),
+	lambdaFn.Permissions = append(lambdaFn.Permissions, sparta.S3Permission{
+		BasePermission: sparta.BasePermission{
+			StatementId: "MyUniqueID",
+			SourceArn:   "arn:aws:s3:::sampleBucket",
+		},
+		// Event Filters are defined at
+		// http://docs.aws.amazon.com/AmazonS3/latest/dev/NotificationHowTo.html
+		Events: []string{"s3:ObjectCreated:*", "s3:ObjectRemoved:*"},
 	})
+
+	lambdaFn.Permissions = append(lambdaFn.Permissions, sparta.SNSPermission{
+		BasePermission: sparta.BasePermission{
+			StatementId: "MyUniqueID",
+			SourceArn:   "arn:aws:sns:us-west-2:000000000000:someTopic",
+		},
+	})
+
 	lambdaFn.EventSourceMappings = append(lambdaFn.EventSourceMappings, &lambda.CreateEventSourceMappingInput{
 		EventSourceArn:   aws.String("arn:aws:dynamodb:us-west-2:000000000000:table/sampleTable"),
 		StartingPosition: aws.String("TRIM_HORIZON"),
 		BatchSize:        aws.Int64(10),
 	})
+
 	lambdaFunctions = append(lambdaFunctions, lambdaFn)
 	lambdaFunctions = append(lambdaFunctions, sparta.NewLambda(LAMBDA_EXECUTE_ARN, mockLambda2, nil))
 	return lambdaFunctions
@@ -70,6 +82,6 @@ func TestDescribe(t *testing.T) {
 	defer output.Close()
 	err = sparta.Describe("SampleService", "SampleService Description", sampleData(), output, logger)
 	if nil != err {
-		t.Errorf("Failed to describe: ", err)
+		t.Errorf("Failed to describe: %s", err)
 	}
 }
