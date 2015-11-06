f653029a3020146306620fa1b7b13ee2c009cefe
Matt Weagle <mweagle@gmail.com>
2015-11-06 08:29:32 -0800

    Fix package-level overview docs

--- doc.go
+++ doc.go
@@ -1,1 +1,11 @@
+/*
+The sparta package transforms a set of golang functions into a deployable package that includes:
+
+	 	1. NodeJS proxy logic
+	 	2. A golang binary
+	 	3. Dynamically generated CloudFormation template that supports create/update & delete operations.
+	 	4. If specified, CloudFormation custom resources to automatically configure S3/SNS push registration
+
+See the Main() docs for more information.
+*/
 package sparta
