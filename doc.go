c62c33a046eaacf95024825a8745dfdd7e5ccda2
Matt Weagle <mweagle@gmail.com>
2015-11-20 11:08:04 -0800

    Doc updates

--- doc.go
+++ doc.go
@@ -1,11 +1,14 @@
 /*
-The sparta package transforms a set of golang functions into a deployable package that includes:
+The sparta package transforms a set of golang functions into an Amazon Lambda deployable unit.
+
+The deployable archive includes
 
 	 	1. NodeJS proxy logic
 	 	2. A golang binary
 	 	3. Dynamically generated CloudFormation template that supports create/update & delete operations.
 	 	4. If specified, CloudFormation custom resources to automatically configure S3/SNS push registration
+		5. If specified, API Gateway provisioning logic via custom resources to make the golang functions publicly accessible.
 
-See the Main() docs for more information.
+See the Main() docs for more information and examples
 */
 package sparta
