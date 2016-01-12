a195c199230e3248c5638a07a0290af8ecbbb771
Matt Weagle <mweagle@gmail.com>
2016-01-12 07:07:50 -0800

    Update `explore` CLI docs

--- explore.go
+++ explore.go
@@ -26,18 +26,19 @@ func Explore(lambdaAWSInfos []*LambdaAWSInfo, port int, logger *logrus.Logger) e
 	}
 
 	for _, eachLambdaInfo := range lambdaPaths {
-		functionPath := fmt.Sprintf("%s/%s", urlHost, eachLambdaInfo.lambdaFnName)
+		functionURL := fmt.Sprintf("%s/%s", urlHost, eachLambdaInfo.lambdaFnName)
 		logger.WithFields(logrus.Fields{
-			"Path": functionPath,
+			"URL": functionURL,
 		}).Info(eachLambdaInfo.lambdaFnName)
 
 		if msgText == "" {
-			msgText = fmt.Sprintf("\tcurl -vs -X POST -H \"Content-Type: application/json\" --data @testEvent.json %s", functionPath)
+			msgText = fmt.Sprintf("\tcurl -vs -X POST -H \"Content-Type: application/json\" --data @testEvent.json %s", functionURL)
 		}
 	}
 	logger.Info("Functions can be invoked via application/json over POST")
 	logger.Info(msgText)
-	logger.Info("Where @testEvent.json is a local file with top level `context` and `event` keys")
+	logger.Info("Where @testEvent.json is a local file with top level `context` and `event` properties:")
+	logger.Info("\t{context: {}, event: {}}")
 	// Start up the localhost server and publish the info
 	return Execute(lambdaAWSInfos, port, 0, logger)
 }
