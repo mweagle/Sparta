8fcb9f60151b3f089736f0286057a74e4c87639e
Matt Weagle <mweagle@gmail.com>
2016-01-13 06:10:40 -0800

    Test custom ReceiptRules

--- doc_sespermission_test.go
+++ doc_sespermission_test.go
@@ -23,13 +23,30 @@ func ExampleSESPermission() {
 	// Create the Lambda
 	sesLambda := NewLambda(roleDefinition, sesLambdaProcessor, nil)
 
-	// Add a Permission s.t. the Lambda function automatically registers for S3 events
-	sesLambda.Permissions = append(sesLambda.Permissions, SESPermission{
+	// Add a Permission s.t. the Lambda function is automatically invoked
+	// in response to inbound email
+	lambdaSESPermission := SESPermission{
 		BasePermission: BasePermission{
 			SourceArn: "*",
 		},
 		InvocationType: "Event",
-	})
+	}
+	// Add some custom ReceiptRules.  Rules will be inserted in this order
+	lambdaSESPermission.ReceiptRules = make([]ReceiptRule, 0)
+	lambdaSESPermission.ReceiptRules = append(lambdaSESPermission.ReceiptRules,
+		ReceiptRule{
+			Name:       "Special",
+			Recipients: []string{"somebody@mydomain.io"},
+			TLSPolicy:  "Optional",
+		})
+
+	lambdaSESPermission.ReceiptRules = append(lambdaSESPermission.ReceiptRules,
+		ReceiptRule{
+			Name:       "Default",
+			Recipients: []string{},
+			TLSPolicy:  "Optional",
+		})
+	sesLambda.Permissions = append(sesLambda.Permissions, lambdaSESPermission)
 
 	lambdaFunctions = append(lambdaFunctions, sesLambda)
 	Main("SESLambdaApp", "Registers for SES events", lambdaFunctions, nil, nil)
