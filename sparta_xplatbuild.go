3fe9cab5c8f58cdd9e2f9067b013cf10f19db5c7
Matt Weagle <mweagle@gmail.com>
2016-01-19 21:49:30 -0800

    Do not reference `syscall.Kill`

--- sparta_xplatbuild.go
+++ sparta_xplatbuild.go
@@ -2,10 +2,8 @@
 
 package sparta
 
-import "syscall"
-
 // Support Windows development, by only requiring `syscall` in the compiled
 // linux binary.
 func platformKill(parentProcessPID int) {
-	syscall.Kill(parentProcessPID, syscall.SIGUSR2)
+	// NOP
 }
