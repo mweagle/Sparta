c2d6534dfaea2dbdd36d254fb9d82cf689cf7db4
Matt Weagle <mweagle@gmail.com>
2016-01-28 05:44:52 -0800

    Use StringExpr rather than map[string]interface{} types for ARN creation

--- aws/cloudformation/util.go
+++ aws/cloudformation/util.go
@@ -1,31 +1,52 @@
 package cloudformation
 
+import (
+	"fmt"
+
+	gocf "github.com/crewjam/go-cloudformation"
+)
+
 // S3AllKeysArnForBucket returns a CloudFormation-compatible Arn expression
 // (string or Ref) for all bucket keys (`/*`).  The bucket
 // parameter may be either a string or an interface{} ("Ref: "myResource")
 // value
-func S3AllKeysArnForBucket(bucket interface{}) map[string]interface{} {
-	return map[string]interface{}{
-		"Fn::Join": []interface{}{"",
-			[]interface{}{
-				"arn:aws:s3:::",
-				bucket,
-				"/*",
-			}}}
+func S3AllKeysArnForBucket(bucket interface{}) *gocf.StringExpr {
+	arnParts := []gocf.Stringable{gocf.String("arn:aws:s3:::")}
+
+	switch bucket.(type) {
+	case string:
+		// Don't be smart if the Arn value is a user supplied literal
+		arnParts = append(arnParts, gocf.String(bucket.(string)))
+	case *gocf.StringExpr:
+		arnParts = append(arnParts, bucket.(*gocf.StringExpr))
+	case gocf.RefFunc:
+		arnParts = append(arnParts, bucket.(gocf.RefFunc).String())
+	default:
+		panic(fmt.Sprintf("Unsupported SourceArn value type: %+v", bucket))
+	}
+	arnParts = append(arnParts, gocf.String("/*"))
+	return gocf.Join("", arnParts...).String()
 }
 
 // S3ArnForBucket returns a CloudFormation-compatible Arn expression
 // (string or Ref) suitable for template reference.  The bucket
 // parameter may be either a string or an interface{} ("Ref: "myResource")
 // value
-func S3ArnForBucket(bucket interface{}) map[string]interface{} {
-	return map[string]interface{}{
-		"Fn::Join": []interface{}{"",
-			[]interface{}{
-				"arn:aws:s3:::",
-				bucket,
-			}}}
+func S3ArnForBucket(bucket interface{}) *gocf.StringExpr {
+	arnParts := []gocf.Stringable{gocf.String("arn:aws:s3:::")}
 
+	switch bucket.(type) {
+	case string:
+		// Don't be smart if the Arn value is a user supplied literal
+		arnParts = append(arnParts, gocf.String(bucket.(string)))
+	case *gocf.StringExpr:
+		arnParts = append(arnParts, bucket.(*gocf.StringExpr))
+	case gocf.RefFunc:
+		arnParts = append(arnParts, bucket.(gocf.RefFunc).String())
+	default:
+		panic(fmt.Sprintf("Unsupported SourceArn value type: %+v", bucket))
+	}
+	return gocf.Join("", arnParts...).String()
 }
 
 // MapToResourceTags transforms a go map[string]string to a CloudFormation-compliant
