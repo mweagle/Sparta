89cddeb9bd10b08e33e331a24a6b6a84c83bb3c6
Matt Weagle <mweagle@gmail.com>
2016-02-15 23:02:42 -1000

    Hook logrus into AWS API calls

--- sparta.go
+++ sparta.go
@@ -20,6 +20,7 @@ import (
 	gocf "github.com/crewjam/go-cloudformation"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/voxelbrain/goptions"
@@ -529,11 +530,28 @@ func sanitizedName(input string) string {
 	return reSanitize.ReplaceAllString(input, "_")
 }
 
+type logrusProxy struct {
+	logger *logrus.Logger
+}
+
+func (proxy *logrusProxy) Log(args ...interface{}) {
+	proxy.logger.Info(args...)
+}
+
 // Returns an AWS Session (https://github.com/aws/aws-sdk-go/wiki/Getting-Started-Configuration)
 // object that attaches a debug level handler to all AWS requests from services
 // sharing the session value.
 func awsSession(logger *logrus.Logger) *session.Session {
-	sess := session.New()
+	awsConfig := &aws.Config{
+		CredentialsChainVerboseErrors: aws.Bool(true),
+	}
+	// Log AWS calls if needed
+	switch logger.Level {
+	case logrus.DebugLevel:
+		awsConfig.LogLevel = aws.LogLevel(aws.LogDebugWithRequestErrors)
+	}
+	awsConfig.Logger = &logrusProxy{logger}
+	sess := session.New(awsConfig)
 	sess.Handlers.Send.PushFront(func(r *request.Request) {
 		logger.WithFields(logrus.Fields{
 			"Service":   r.ClientInfo.ServiceName,
@@ -543,6 +561,12 @@ func awsSession(logger *logrus.Logger) *session.Session {
 			"Payload":   r.Params,
 		}).Debug("AWS Request")
 	})
+
+	logger.WithFields(logrus.Fields{
+		"Name":    aws.SDKName,
+		"Version": aws.SDKVersion,
+	}).Debug("AWS SDK Info")
+
 	return sess
 }
 
