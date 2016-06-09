ba47acef2a7c5216de6a71d6ad805ea0df746fa6
Matt Weagle <mweagle@gmail.com>
2016-06-08 21:01:46 -0700

    Use new sparta/aws.NewSession() function

--- delete.go
+++ delete.go
@@ -6,12 +6,13 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
+	spartaAWS "github.com/mweagle/Sparta/aws"
 )
 
 // Delete the provided serviceName.  Failing to delete a non-existent
 // service is not considered an error.  Note that the delete does
 func Delete(serviceName string, logger *logrus.Logger) error {
-	session := awsSession(logger)
+	session := spartaAWS.NewSession(logger)
 	awsCloudFormation := cloudformation.New(session)
 
 	exists, err := stackExists(serviceName, awsCloudFormation, logger)
