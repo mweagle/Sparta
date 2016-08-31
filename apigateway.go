5c8ccb4c37f1a9fda62f195fb68d75799b154c36
Matt Weagle <mweagle@gmail.com>
2016-08-30 23:47:13 -0700

    ## BREAKING CHANGE
    Support user-defined HTTP headers to be returned from API Gateway.  Prior to this, the response maps were only populated at `API.export()`. Update initialization so that they are populated as part of `NewLambda`.
    
    Usage:
    ```
    type helloWorldResponse struct {
    	Location string `json:"location"`
    	Body     string `json:"body"`
    }
    
    ...
    
    apiGatewayResource, _ := apiGateway.NewResource("/helloWorld", lambdaFn)
    apiGWMethod, _ := apiGatewayResource.NewMethod("GET", 200)
    apiGWMethod.Responses[200].Parameters = map[string]bool{
    	"method.response.header.Location": true,
    }
    apiGWMethod.Integration.Responses[200].Parameters["method.response.header.Location"] = "integration.response.body.location"
    
    ```

--- apigateway.go
+++ apigateway.go
@@ -2,7 +2,6 @@ package sparta
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -116,7 +115,7 @@ type APIGatewayLambdaJSONEvent struct {
 }
 
 // Model proxies the AWS SDK's Model data.  See
-// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#type-Model
+// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#Model
 //
 // TODO: Support Dynamic Model creation
 type Model struct {
@@ -126,14 +125,14 @@ type Model struct {
 }
 
 // Response proxies the AWS SDK's PutMethodResponseInput data.  See
-// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#type-PutMethodResponseInput
+// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#PutMethodResponseInput
 type Response struct {
 	Parameters map[string]bool   `json:",omitempty"`
 	Models     map[string]*Model `json:",omitempty"`
 }
 
 // IntegrationResponse proxies the AWS SDK's IntegrationResponse data.  See
-// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#type-IntegrationResponse
+// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway/#IntegrationResponse
 type IntegrationResponse struct {
 	Parameters       map[string]string `json:",omitempty"`
 	SelectionPattern string            `json:",omitempty"`
@@ -141,7 +140,7 @@ type IntegrationResponse struct {
 }
 
 // Integration proxies the AWS SDK's Integration data.  See
-// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#type-Integration
+// http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#Integration
 type Integration struct {
 	Parameters         map[string]string
 	RequestTemplates   map[string]string
@@ -158,9 +157,11 @@ type Integration struct {
 // Method proxies the AWS SDK's Method data.  See
 // http://docs.aws.amazon.com/sdk-for-go/api/service/apigateway.html#type-Method
 type Method struct {
-	authorizationType string
-	httpMethod        string
-	APIKeyRequired    bool
+	authorizationType       string
+	httpMethod              string
+	defaultHTTPResponseCode int
+
+	APIKeyRequired bool
 
 	// Request data
 	Parameters map[string]bool
@@ -210,56 +211,40 @@ type API struct {
 	CORSEnabled bool
 }
 
-func corsMethodResponseParams() map[string]string {
-	responseParams := make(map[string]string, 0)
-	responseParams["method.response.header.Access-Control-Allow-Headers"] = boolTrue
-	responseParams["method.response.header.Access-Control-Allow-Methods"] = boolTrue
-	responseParams["method.response.header.Access-Control-Allow-Origin"] = boolTrue
+func corsMethodResponseParams() map[string]bool {
+	responseParams := make(map[string]bool, 0)
+	responseParams["method.response.header.Access-Control-Allow-Headers"] = true
+	responseParams["method.response.header.Access-Control-Allow-Methods"] = true
+	responseParams["method.response.header.Access-Control-Allow-Origin"] = true
 	return responseParams
 }
 
 // DefaultMethodResponses returns the default set of Method HTTPStatus->Response
 // pass through responses.  The successfulHTTPStatusCode param is the single
 // 2XX response code to use for the method.
-func methodResponses(successfulHTTPStatusCode int,
-	userResponses map[int]*Response,
+func methodResponses(userResponses map[int]*Response,
 	corsEnabled bool) *gocf.APIGatewayMethodMethodResponseList {
 
 	var responses gocf.APIGatewayMethodMethodResponseList
-	if len(userResponses) != 0 {
-		for eachStatusCode := range userResponses {
-			methodResponse := gocf.APIGatewayMethodMethodResponse{
-				StatusCode: gocf.String(strconv.Itoa(eachStatusCode)),
-			}
-			if corsEnabled {
-				methodResponse.ResponseParameters = corsMethodResponseParams()
-			}
-			responses = append(responses, methodResponse)
-		}
-	} else {
-		// Add the single successful response
-		methodResponse := gocf.APIGatewayMethodMethodResponse{
-			StatusCode: gocf.String(strconv.Itoa(successfulHTTPStatusCode)),
-		}
+	for eachHTTPStatusCode, eachResponse := range userResponses {
+		methodResponseParams := eachResponse.Parameters
 		if corsEnabled {
-			methodResponse.ResponseParameters = corsMethodResponseParams()
+			for eachString, eachBool := range corsMethodResponseParams() {
+				methodResponseParams[eachString] = eachBool
+			}
+		}
+		// Then transform them all to strings because internet
+		methodResponseStringParams := make(map[string]string, len(methodResponseParams))
+		for eachKey, eachBool := range methodResponseParams {
+			methodResponseStringParams[eachKey] = fmt.Sprintf("%t", eachBool)
+		}
+		methodResponse := gocf.APIGatewayMethodMethodResponse{
+			StatusCode: gocf.String(strconv.Itoa(eachHTTPStatusCode)),
+		}
+		if len(methodResponseStringParams) != 0 {
+			methodResponse.ResponseParameters = methodResponseStringParams
 		}
 		responses = append(responses, methodResponse)
-
-		for i := 300; i <= 599; i++ {
-			statusText := http.StatusText(i)
-			if "" != statusText {
-
-				methodResponse := gocf.APIGatewayMethodMethodResponse{
-					StatusCode: gocf.String(strconv.Itoa(i)),
-				}
-				// TODO - handle user defined params
-				if corsEnabled {
-					methodResponse.ResponseParameters = corsMethodResponseParams()
-				}
-				responses = append(responses, methodResponse)
-			}
-		}
 	}
 	return &responses
 }
@@ -272,34 +257,32 @@ func corsIntegrationResponseParams() map[string]string {
 	return responseParams
 }
 
-func integrationResponses(successfulHTTPStatusCode int,
-	userResponses map[int]*IntegrationResponse,
+func integrationResponses(userResponses map[int]*IntegrationResponse,
 	corsEnabled bool) *gocf.APIGatewayMethodIntegrationIntegrationResponseList {
 
-	// TODO - userResponses
 	var integrationResponses gocf.APIGatewayMethodIntegrationIntegrationResponseList
-	for i := 200; i <= 599; i++ {
-		statusText := http.StatusText(i)
-		if "" != statusText {
-			regExp := fmt.Sprintf(".*%s.*", statusText)
-			if i == successfulHTTPStatusCode {
-				regExp = ""
+
+	// We've already populated this entire map in the NewMethod call
+	for eachHTTPStatusCode, eachMethodIntegrationResponse := range userResponses {
+
+		responseParameters := eachMethodIntegrationResponse.Parameters
+		if corsEnabled {
+			for eachKey, eachValue := range corsIntegrationResponseParams() {
+				responseParameters[eachKey] = eachValue
 			}
-			integrationResponse := gocf.APIGatewayMethodIntegrationIntegrationResponse{
-				ResponseTemplates: map[string]string{
-					"application/json": "",
-					"text/*":           "",
-				},
-				SelectionPattern: gocf.String(regExp),
-				StatusCode:       gocf.String(strconv.Itoa(i)),
-			}
-			// TODO - handle user defined params
-			if corsEnabled {
-				integrationResponse.ResponseParameters = corsIntegrationResponseParams()
-			}
-			integrationResponses = append(integrationResponses, integrationResponse)
 		}
+
+		integrationResponse := gocf.APIGatewayMethodIntegrationIntegrationResponse{
+			ResponseTemplates: eachMethodIntegrationResponse.Templates,
+			SelectionPattern:  gocf.String(eachMethodIntegrationResponse.SelectionPattern),
+			StatusCode:        gocf.String(strconv.Itoa(eachHTTPStatusCode)),
+		}
+		if len(responseParameters) != 0 {
+			integrationResponse.ResponseParameters = responseParameters
+		}
+		integrationResponses = append(integrationResponses, integrationResponse)
 	}
+
 	return &integrationResponses
 }
 
@@ -490,19 +473,13 @@ func (api *API) export(serviceName string,
 		// Then if the api is CORS enabled, setup the options method
 		if api.CORSEnabled {
 			methodResourceName := CloudFormationResourceName(fmt.Sprintf("%s-OPTIONS", eachResourcePath), eachResourcePath)
-			template.AddResource(methodResourceName,
-				corsOptionsGatewayMethod(apiGatewayRestAPIID, parentResource))
-
+			template.AddResource(methodResourceName, corsOptionsGatewayMethod(apiGatewayRestAPIID, parentResource))
 			apiMethodCloudFormationResources = append(apiMethodCloudFormationResources, methodResourceName)
 		}
 		// END CORS - OPTIONS verb
 
 		// BEGIN - user defined verbs
 		for eachMethodName, eachMethodDef := range eachResourceDef.Methods {
-			statusSuccessfulCode := http.StatusOK
-			if eachMethodDef.httpMethod == "POST" {
-				statusSuccessfulCode = http.StatusCreated
-			}
 
 			apiGatewayMethod := &gocf.ApiGatewayMethod{
 				HttpMethod:        gocf.String(eachMethodName),
@@ -523,10 +500,12 @@ func (api *API) export(serviceName string,
 			}
 
 			// Add the integration response RegExps
-			apiGatewayMethod.Integration.IntegrationResponses = integrationResponses(statusSuccessfulCode, eachMethodDef.Integration.Responses, api.CORSEnabled)
+			apiGatewayMethod.Integration.IntegrationResponses = integrationResponses(eachMethodDef.Integration.Responses,
+				api.CORSEnabled)
 
 			// Add outbound method responses
-			apiGatewayMethod.MethodResponses = methodResponses(statusSuccessfulCode, eachMethodDef.Responses, api.CORSEnabled)
+			apiGatewayMethod.MethodResponses = methodResponses(eachMethodDef.Responses,
+				api.CORSEnabled)
 
 			prefix := fmt.Sprintf("%s%s", eachMethodDef.httpMethod, eachResourcePath)
 			methodResourceName := CloudFormationResourceName(prefix, eachResourcePath, serviceName)
@@ -633,16 +612,19 @@ func (api *API) NewResource(pathPart string, parentLambda *LambdaAWSInfo) (*Reso
 
 // NewMethod associates the httpMethod name with the given Resource.  The returned Method
 // has no authorization requirements.
-func (resource *Resource) NewMethod(httpMethod string) (*Method, error) {
+func (resource *Resource) NewMethod(httpMethod string, defaultHTTPStatusCode int) (*Method, error) {
 	authorizationType := "NONE"
 
 	// http://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-method-settings.html#how-to-method-settings-console
 	keyname := httpMethod
 	_, exists := resource.Methods[keyname]
 	if exists {
-		errMsg := fmt.Sprintf("Method %s (Auth: %s) already defined for resource", httpMethod, authorizationType)
-		return nil, errors.New(errMsg)
+		return nil, fmt.Errorf("Method %s (Auth: %s) already defined for resource", httpMethod, authorizationType)
 	}
+	if 0 == defaultHTTPStatusCode {
+		return nil, fmt.Errorf("Invalid default HTTP status (%d) code for method", defaultHTTPStatusCode)
+	}
+
 	integration := Integration{
 		Parameters:       make(map[string]string, 0),
 		RequestTemplates: make(map[string]string, 0),
@@ -651,20 +633,53 @@ func (resource *Resource) NewMethod(httpMethod string) (*Method, error) {
 	}
 
 	method := &Method{
-		authorizationType: authorizationType,
-		httpMethod:        httpMethod,
-		Parameters:        make(map[string]bool, 0),
-		Models:            make(map[string]*Model, 0),
-		Responses:         make(map[int]*Response, 0),
-		Integration:       integration,
+		authorizationType:       authorizationType,
+		httpMethod:              httpMethod,
+		defaultHTTPResponseCode: defaultHTTPStatusCode,
+		Parameters:              make(map[string]bool, 0),
+		Models:                  make(map[string]*Model, 0),
+		Responses:               make(map[int]*Response, 0),
+		Integration:             integration,
 	}
+
+	// Populate Integration.Responses and the method Parameters
+	for i := http.StatusOK; i <= http.StatusNetworkAuthenticationRequired; i++ {
+		statusText := http.StatusText(i)
+		if "" != statusText {
+			// First the Integration Responses...
+			regExp := fmt.Sprintf(".*%s.*", statusText)
+			if defaultHTTPStatusCode == i {
+				regExp = ""
+			}
+			method.Integration.Responses[i] = &IntegrationResponse{
+				Parameters: make(map[string]string, 0),
+				Templates: map[string]string{
+					"application/json": "",
+					"text/*":           "",
+				},
+				SelectionPattern: regExp,
+			}
+
+			// Then the Method.Responses
+			method.Responses[i] = &Response{
+				Parameters: make(map[string]bool, 0),
+				Models:     make(map[string]*Model, 0),
+			}
+		}
+	}
+
+	// apiGWMethod.Responses[200].Parameters = map[string]bool{
+	// 	"method.response.header.Location": true,
+	// }
+	// apiGWMethod.Integration.Responses[200].Parameters["method.response.header.Location"] = "integration.response.body.location"
+
 	resource.Methods[keyname] = method
 	return method, nil
 }
 
 // NewAuthorizedMethod associates the httpMethod name and authorizationType with the given Resource.
-func (resource *Resource) NewAuthorizedMethod(httpMethod string, authorizationType string) (*Method, error) {
-	method, err := resource.NewMethod(httpMethod)
+func (resource *Resource) NewAuthorizedMethod(httpMethod string, authorizationType string, defaultHTTPStatusCode int) (*Method, error) {
+	method, err := resource.NewMethod(httpMethod, defaultHTTPStatusCode)
 	if nil != err {
 		method.authorizationType = authorizationType
 	}
