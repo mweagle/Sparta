964ac0ddceac8bea830d2c71efc100ac56341b6d
Matt Weagle <mweagle@gmail.com>
2016-10-06 20:57:13 -0700

    Automatic updates

--- CONSTANTS_AWSBINARY.go
+++ CONSTANTS_AWSBINARY.go
@@ -193,7 +193,7 @@ var _escData = map[string]*_escFile{
 	"/resources/awsbinary/README.md": {
 		local:   "./resources/awsbinary/README.md",
 		size:    104,
-		modtime: 1473345217,
+		modtime: 1475032722,
 		compressed: `
 H4sIAAAJbogA/yTLQQrCMBAF0L2n+BfQOxQ3CloXE3Ap0+QXA0mndMZFby/oAV56V8dcG1EdtrQdH2dB
 GPJGDUJBz8dsfdWoUyPOj1HSMCZ5yeV6lz/+odk2DE/BTftUFIVrs71ziRMO3wAAAP//AOMpDWgAAAA=
