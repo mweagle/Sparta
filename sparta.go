be5c7ca263b65a468135f771de66fd19b39bdeb7
Matt Weagle <mweagle@gmail.com>
2016-10-08 10:23:59 -0700

    Bump Sparta version

--- sparta.go
+++ sparta.go
@@ -30,7 +30,7 @@ import (
 
 const (
 	// SpartaVersion defines the current Sparta release
-	SpartaVersion = "0.9.1"
+	SpartaVersion = "0.9.2"
 	// NodeJSVersion is the Node JS runtime used for the shim layer
 	NodeJSVersion = "nodejs4.3"
 
