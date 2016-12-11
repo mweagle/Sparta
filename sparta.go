61945ef8e7affe37be068c092ea7b416d8cd7b8e
Matt Weagle <mweagle@gmail.com>
2016-12-11 08:12:21 -0800

    Bump Sparta release version

--- sparta.go
+++ sparta.go
@@ -30,7 +30,7 @@ import (
 
 const (
 	// SpartaVersion defines the current Sparta release
-	SpartaVersion = "0.9.4"
+	SpartaVersion = "0.10.0"
 	// NodeJSVersion is the Node JS runtime used for the shim layer
 	NodeJSVersion = "nodejs4.3"
 	// Custom Resource typename used to create new cloudFormationUserDefinedFunctionCustomResource
