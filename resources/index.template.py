5710a3482eb43e559d0066742637a2ce5fb6f39d
Matt Weagle <mweagle@gmail.com>
2017-04-14 00:08:47 -0700

    Cleanup Python ctypes to handle returned content-types

--- resources/index.template.py
+++ resources/index.template.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import pprint
 import json
+import sys
 from ctypes import *
 
 lib = cdll.LoadLibrary("{{ .LibraryName }}")
@@ -8,6 +9,7 @@ lib.Lambda.argtypes = [c_char_p,
                         c_char_p,
                         POINTER(c_int),
                         c_char_p,
+                        c_int,
                         c_char_p,
                         c_int]
 lib.Lambda.restype = c_int
@@ -22,63 +24,65 @@ MAX_RESPONSE_CONTENT_TYPE_SIZE = 1024
 response_content_type_buffer = create_string_buffer(MAX_RESPONSE_CONTENT_TYPE_SIZE)
 
 def lambda_handler(funcName, event, context):
-    # Need to marshall the string into something we can get to in the
-    # Go universe, so for that we can just get a struct
-    # with the context. The event content can be passed in as a
-    # raw char pointer.
-    request = {
-        "event" : event,
-        "context" : {}
-    }
-    contextDict = {}
-    contextDict["functionName"] = context.function_name
-    contextDict["functionVersion"] = context.function_version
-    contextDict["invokedFunctionArn"] = context.invoked_function_arn
-    contextDict["memoryLimitInMB"] = context.memory_limit_in_mb
-    contextDict["awsRequestId"] = context.aws_request_id
-    contextDict["logGroupName"] = context.log_group_name
-    contextDict["logStreamName"] = context.log_stream_name
-
-    # Identity check...
-    if context.identity is not None:
-        contextDict["identity"] = {
-            "cognitoIdentityId" : context.identity.cognito_identity_id,
-            "cognitoIdentityPoolId" : context.identity.cognito_identity_pool_id
+    try:
+        # Need to marshall the string into something we can get to in the
+        # Go universe, so for that we can just get a struct
+        # with the context. The event content can be passed in as a
+        # raw char pointer.
+        request = {
+            "event" : event,
+            "context" : {}
         }
-    # Client context
-    if context.client_context is not None:
-        destClientContext = {}
-        srcClientContext = context.client_context
-        destClientContext["installation_id"] = context.client_context.installation_id
-        destClientContext["app_title"] = context.client_context.app_title
-        destClientContext["app_version_name"] = context.client_context.app_version_name
-        destClientContext["app_version_code"] = context.client_context.app_version_code
-        destClientContext["Custom"] = context.client_context.custom
-        destClientContext["env"] = context.client_context.env
-        contextDict["client_context"] = destClientContext
+        contextDict = {}
+        contextDict["functionName"] = context.function_name
+        contextDict["functionVersion"] = context.function_version
+        contextDict["invokedFunctionArn"] = context.invoked_function_arn
+        contextDict["memoryLimitInMB"] = context.memory_limit_in_mb
+        contextDict["awsRequestId"] = context.aws_request_id
+        contextDict["logGroupName"] = context.log_group_name
+        contextDict["logStreamName"] = context.log_stream_name
 
-    # Update it
-    request["context"] = contextDict
-    memset(response_buffer, 0, MAX_RESPONSE_SIZE)
-    memset(response_content_type, 0, 2014)
-    exitCode = c_int()
-    bytesWritten = lib.Lambda(funcName.encode('utf-8'),
-                                json.dumps(request),
-                                byref(exitCode),
-                                response_content_type_buffer,
-                                MAX_RESPONSE_CONTENT_TYPE_SIZE,
-                                response_buffer,
-                                MAX_RESPONSE_SIZE-1)
+        # Identity check...
+        if context.identity is not None:
+            contextDict["identity"] = {
+                "cognitoIdentityId" : context.identity.cognito_identity_id,
+                "cognitoIdentityPoolId" : context.identity.cognito_identity_pool_id
+            }
+        # Client context
+        if context.client_context is not None:
+            destClientContext = {}
+            srcClientContext = context.client_context
+            destClientContext["installation_id"] = context.client_context.installation_id
+            destClientContext["app_title"] = context.client_context.app_title
+            destClientContext["app_version_name"] = context.client_context.app_version_name
+            destClientContext["app_version_code"] = context.client_context.app_version_code
+            destClientContext["Custom"] = context.client_context.custom
+            destClientContext["env"] = context.client_context.env
+            contextDict["client_context"] = destClientContext
 
-    lowercase_content_type = response_content_type_buffer.lower()
-    if "json" in lowercase_content_type:
-        return json.loads(response_buffer.value)
-    elif "octet-stream" in lowercase_content_type:
-        return bytearray(response_buffer.value)
-    elif "binary" in lowercase_content_type:
-        return bytearray(response_buffer.value)
-    else:
-        return response_buffer.value
+        # Update it
+        request["context"] = contextDict
+        memset(response_buffer, 0, MAX_RESPONSE_SIZE)
+        memset(response_content_type_buffer, 0, MAX_RESPONSE_CONTENT_TYPE_SIZE)
+        exitCode = c_int()
+        bytesWritten = lib.Lambda(funcName.encode('utf-8'),
+                                    json.dumps(request),
+                                    byref(exitCode),
+                                    response_content_type_buffer,
+                                    MAX_RESPONSE_CONTENT_TYPE_SIZE,
+                                    response_buffer,
+                                    MAX_RESPONSE_SIZE-1)
+        lowercase_content_type = response_content_type_buffer.value.lower()
+        if "json" in lowercase_content_type:
+            return json.loads(response_buffer.value)
+        elif "octet-stream" in lowercase_content_type:
+            return bytearray(response_buffer.value)
+        elif "binary" in lowercase_content_type:
+            return bytearray(response_buffer.value)
+        else:
+            return response_buffer.value
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
 ## Insert auto generated code here...
 {{ .PythonFunctions }}
