d0288d2602463cee48a853472f31b95ad6d2b9d7
Matt Weagle <mweagle@gmail.com>
2017-09-11 23:15:30 -0700

    Update typename

--- execute.go
+++ execute.go
@@ -27,7 +27,7 @@ func Execute(lambdaAWSInfos []*LambdaAWSInfo, port int, parentProcessPID int, lo
 
 	server := &http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
-		Handler:      NewLambdaHTTPHandler(lambdaAWSInfos, logger),
+		Handler:      NewServeMuxLambda(lambdaAWSInfos, logger),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
