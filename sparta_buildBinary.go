335ce6dc35c1aae1addddcb77548c108d9d03e85
Matt Weagle <mweagle@gmail.com>
2017-09-18 05:35:05 -0700

    Update godocs

--- sparta_buildBinary.go
+++ sparta_buildBinary.go
@@ -15,7 +15,12 @@ func init() {
 }
 
 // RegisterCodePipelineEnvironment is part of a CodePipeline deployment
-// and defines the environments available for deployment
+// and defines the environments available for deployment. Environments
+// are defined the `environmentName`. The values defined in the
+// environmentVariables are made available to each service as
+// environment variables. The environment key will be transformed into
+// a configuration file for a CodePipeline CloudFormation action:
+// TemplateConfiguration: !Sub "TemplateSource::${environmentName}".
 func RegisterCodePipelineEnvironment(environmentName string, environmentVariables map[string]string) error {
 	if _, exists := codePipelineEnvironments[environmentName]; exists {
 		return fmt.Errorf("Environment (%s) has already been defined", environmentName)
