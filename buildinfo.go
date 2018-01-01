f19fb9ac63566f3c7d4f6d80f42952121074ca0c
Matt Weagle <mweagle@gmail.com>
2018-01-01 14:28:23 -0800

    Publishing latest autogenerated updates

--- buildinfo.go
+++ buildinfo.go
@@ -4,4 +4,4 @@ package sparta
 // DO NOT EDIT
 
 // SpartaGitHash is the commit hash of this Sparta library
-const SpartaGitHash = "8f97d8192f498b0094706be643d89e97644440f2"
+const SpartaGitHash = "d796ff2fad782508f4dbbc6329b15e1ea3466474"
