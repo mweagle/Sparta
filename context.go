96b0943b247108c0f638d82064acbd77725d8745
Matt Weagle <mweagle@gmail.com>
2018-01-13 21:18:21 -0800

    Define new context keys

--- context.go
+++ context.go
@@ -3,10 +3,21 @@ package sparta
 type contextKey int
 
 const (
-	// ContextKeyLogger is the *logrus.Logger instance
-	// attached to the request context
+	// ContextKeyLogger is the request-independent *logrus.Logger
+	// instance common to all requests
 	ContextKeyLogger contextKey = iota
+	// ContextKeyRequestLogger is the *logrus.Entry instance
+	// that is annotated with request-identifying
+	// information extracted from the AWS context object
+	ContextKeyRequestLogger
 	// ContextKeyLambdaContext is the *sparta.LambdaContext
 	// pointer in the request
+	// DEPRECATED
 	ContextKeyLambdaContext
+)
+
+const (
+	// ContextKeyLambdaVersions is the key in the context that stores the map
+	// of autoincrementing versions
+	ContextKeyLambdaVersions = "spartaLambdaVersions"
 )
