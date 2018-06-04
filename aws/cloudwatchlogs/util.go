fadb600b6ad83a9884e1bd1e44cc49ac513ae103
Matt Weagle <mweagle@gmail.com>
2018-06-03 18:05:01 -0700

    Add utility function to tail CW LogGroupName

---
+++ aws/cloudwatchlogs/util.go
@@ -0,0 +1,79 @@
+package cloudwatchlogs
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+)
+
+func tailParams(logGroupName string, filter string, lastEvent int64) *cloudwatchlogs.FilterLogEventsInput {
+	params := &cloudwatchlogs.FilterLogEventsInput{
+		LogGroupName: aws.String(logGroupName),
+	}
+	if filter != "" {
+		params.FilterPattern = aws.String(filter)
+	}
+	if lastEvent != 0 {
+		params.StartTime = aws.Int64(lastEvent)
+	}
+	return params
+}
+
+// TailWithContext is a utiltiy function that support tailing the given log stream
+// name using the optional filter. It returns a channel for log messages
+func TailWithContext(reqContext aws.Context,
+	closeChan chan bool,
+	awsSession *session.Session,
+	logGroupName string,
+	filter string,
+	logger *logrus.Logger) <-chan *cloudwatchlogs.FilteredLogEvent {
+
+	// Milliseconds...
+	lastSeenTimestamp := time.Now().Add(0).Unix() * 1000
+	logger.WithField("TS", lastSeenTimestamp).Debug("Started polling")
+	outputChannel := make(chan *cloudwatchlogs.FilteredLogEvent)
+	tailHandler := func(res *cloudwatchlogs.FilterLogEventsOutput, lastPage bool) bool {
+		maxTime := int64(0)
+		for _, eachEvent := range res.Events {
+			if maxTime < *eachEvent.Timestamp {
+				maxTime = *eachEvent.Timestamp
+			}
+			logger.WithField("ID", *eachEvent.EventId).Debug("Event")
+			outputChannel <- eachEvent
+		}
+		if maxTime != 0 {
+			lastSeenTimestamp = maxTime + 1
+		}
+		return !lastPage
+	}
+
+	cwlogsSvc := cloudwatchlogs.New(awsSession)
+	tickerChan := time.NewTicker(time.Millisecond * 333).C //AWS cloudwatch logs limit is 5tx/sec
+	go func() {
+		for {
+			select {
+			case <-closeChan:
+				logger.Debug("Exiting polling loop")
+				return
+			case <-tickerChan:
+				logParam := tailParams(logGroupName, filter, lastSeenTimestamp)
+				error := cwlogsSvc.FilterLogEventsPagesWithContext(reqContext, logParam, tailHandler)
+				if error != nil {
+					if awsErr, ok := error.(awserr.Error); ok {
+						fmt.Println(awsErr.Message())
+						os.Exit(1)
+					}
+				}
+			}
+		}
+	}()
+
+	return outputChannel
+}
