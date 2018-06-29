63ef2e5a2bc20646b8754375cd66518775fdd004
Matt Weagle <mweagle@gmail.com>
2018-06-29 06:10:23 -0700

    Bump version

--- sparta.go
+++ sparta.go
@@ -31,7 +31,7 @@ import (
 
 const (
 	// SpartaVersion defines the current Sparta release
-	SpartaVersion = "1.1.1"
+	SpartaVersion = "1.2.0"
 	// GoLambdaVersion is the Go version runtime used for the lambda function
 	GoLambdaVersion = "go1.x"
 	// SpartaBinaryName is binary name that exposes the Go lambda function
