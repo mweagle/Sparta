6e7028d4fbef1b3ed33f9c14ee3835725c9bf996
Matt Weagle <mweagle@gmail.com>
2018-10-01 20:26:22 -0700

    Add package docs

---
+++ aws/cloudformation/cli/doc.go
@@ -0,0 +1,4 @@
+/*Package main provides a simple cli tool to produce a DescribeStackResponse
+for a given CloudFormation Stack Name or ID.
+*/
+package main
---
+++ aws/cloudformation/resources/doc.go
@@ -0,0 +1,5 @@
+/*Package resources defines a set of CloudFormation Custom Resources that
+are implemented by Lambda functions. These resources are serialized to JSON
+and are instantiated by Sparta when an application requests a CustomAction
+handler. They must implement CustomResourceCommand to be properly invoked. */
+package resources
