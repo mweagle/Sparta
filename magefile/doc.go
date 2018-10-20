f33235ed34c4a53b2404e5e294954025633ed0b3
Matt Weagle <mweagle@gmail.com>
2018-10-20 08:35:37 -0700

    Add magefile doc

--- magefile/doc.go
+++ magefile/doc.go
@@ -1,1 +1,6 @@
+/**Package magefile exposes Mage (https://magefile.org) compatible
+functions that can be re-used by embedding applications. See https://github.com/mweagle/SpartaHelloWorld/blob/master/magefile.go
+for an example.
+**/
+
 package magefile
