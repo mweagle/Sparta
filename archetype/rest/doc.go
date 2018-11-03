a2d326c63e22ab51d81fe37abc827f1ca0df0bd5
Matt Weagle <mweagle@gmail.com>
2018-11-03 09:39:36 -0700

    Add package docs

---
+++ archetype/rest/doc.go
@@ -0,0 +1,6 @@
+/*
+Package rest provides a set of utility functions to make
+building REST-based services simpler. See the https://github.com/mweagle/SpartaTodoBackend
+project for a complete example.
+*/
+package rest
