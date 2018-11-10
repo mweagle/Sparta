b2ab1213ba1cb1d678193c1dce500d7777aae3bd
Matt Weagle <mweagle@gmail.com>
2018-11-10 15:52:42 -0800

    Cleanup comment, formatting

--- interceptor/xray_awsbinary.go
+++ interceptor/xray_awsbinary.go
@@ -19,13 +19,13 @@ const (
 )
 
 func (xri *xrayInterceptor) Begin(ctx context.Context, msg json.RawMessage) context.Context {
+
 	// Put this into the context...
 	segmentCtx, segment := xray.BeginSubsegment(ctx, "Sparta")
 
 	// Add some xray annotations to help track this version
 	// of the service
 	// https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-go-segment.html
-	// TODO
 	if segment != nil {
 		errAddAnnotation := segment.AddAnnotation(XRayAttrBuildID, sparta.StampedBuildID)
 		if errAddAnnotation != nil {
@@ -41,11 +41,12 @@ func (xri *xrayInterceptor) BeforeSetup(ctx context.Context, msg json.RawMessage
 }
 
 func (xri *xrayInterceptor) AfterSetup(ctx context.Context, msg json.RawMessage) context.Context {
-	if xri.mode&XRayModeCaptureLogs != 0 {
+	if xri.mode&XRayModeErrCaptureLogs != 0 {
 		logger, loggerOk := ctx.Value(sparta.ContextKeyLogger).(*logrus.Logger)
 		if loggerOk {
 			// So we need a loggerWrapper that has the debug level turned on.
-			// That will forward things to  the other logger
+			// This filtering formatter will put everything in a logring and
+			// flush iff there is an error
 			xri.filteringFormatter = &filteringFormatter{
 				targetFormatter: logger.Formatter,
 				originalLevel:   logger.Level,
@@ -78,21 +79,25 @@ func (xri *xrayInterceptor) Complete(ctx context.Context, msg json.RawMessage) c
 		if segmentOk {
 			errValue, errValueOk := ctx.Value(sparta.ContextKeyLambdaError).(error)
 			if errValueOk && errValue != nil {
-				metadataEventErr := segment.AddMetadataToNamespace(sparta.ProperName, XRayMetadataErrValue, errValue.Error())
-				if metadataEventErr != nil {
-					log.Printf("Failed to set event %s metadata: %s", XRayMetadataErrValue, metadataEventErr)
+
+				// Include the error value?
+				if xri.mode&XRayModeErrCaptureErrorValue != 0 {
+					metadataEventErr := segment.AddMetadataToNamespace(sparta.ProperName, XRayMetadataErrValue, errValue.Error())
+					if metadataEventErr != nil {
+						log.Printf("Failed to set event %s metadata: %s", XRayMetadataErrValue, metadataEventErr)
+					}
 				}
 
 				// Include the event?
-				if xri.mode&XRayModeCaptureEvent != 0 {
+				if xri.mode&XRayModeErrCaptureEvent != 0 {
 					metadataEventErr = segment.AddMetadataToNamespace(sparta.ProperName, XRayMetadataErrEvent, msg)
 					if metadataEventErr != nil {
 						log.Printf("Failed to set event %s metadata: %s", XRayMetadataErrEvent, metadataEventErr)
 					}
 				}
 
-				// Include the request ID...
-				if xri.mode&XRayModeCaptureRequestID != 0 {
+				// Include the request ID?
+				if xri.mode&XRayModeErrCaptureRequestID != 0 {
 					awsContext, _ := lambdacontext.FromContext(ctx)
 					if awsContext != nil {
 						metadataEventErr = segment.AddMetadataToNamespace(sparta.ProperName, XRayMetadataRequestID, awsContext.AwsRequestID)
@@ -103,7 +108,7 @@ func (xri *xrayInterceptor) Complete(ctx context.Context, msg json.RawMessage) c
 				}
 
 				// Include the cached log info?
-				if xri.mode&XRayModeCaptureLogs != 0 {
+				if xri.mode&XRayModeErrCaptureLogs != 0 {
 					logFormatter := &logrus.JSONFormatter{}
 					logMessages := make([]map[string]*json.RawMessage, 0)
 					xri.filteringFormatter.logRing.Do(func(eachLogEntry interface{}) {
