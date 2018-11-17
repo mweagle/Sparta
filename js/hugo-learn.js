2b67585e5bc62efc1b2ee07d8fb968a59a7913ef
Matt Weagle <mweagle@gmail.com>
2018-11-17 06:59:39 -0800

    "Updated documentation"

--- js/hugo-learn.js
+++ js/hugo-learn.js
@@ -68,7 +68,7 @@ jQuery(document).ready(function() {
   var text, clip = new Clipboard('.anchor');
   $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function(index, html){
     var element = $(this);
-    var url = document.location.origin + document.location.pathname;
+    var url = encodeURI(document.location.origin + document.location.pathname);
     var link = url + "#"+element[0].id;
     return " <span class='anchor' data-clipboard-text='"+link+"'>" +
       "<i class='fas fa-link fa-lg'></i>" +
