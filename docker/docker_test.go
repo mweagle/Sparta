18b4f76f43cceefc49025e616f00ed653e50149f
Matt Weagle <mweagle@gmail.com>
2018-12-09 16:30:40 -0800

    Add test for docker password via STDIN

---
+++ docker/docker_test.go
@@ -0,0 +1,45 @@
+package docker
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/mweagle/Sparta"
+	"github.com/mweagle/Sparta/system"
+)
+
+func TestLogin(t *testing.T) {
+	logger, _ := sparta.NewLogger("info")
+
+	// If docker -v doesn't work, then login definitely won't
+	dockerVersionCmd := exec.Command("docker", "-v")
+	dockerVersionCmdErr := system.RunOSCommand(dockerVersionCmd, logger)
+	if dockerVersionCmdErr != nil {
+		return
+	}
+	// Don't supply password on command line
+
+	dockerLoginCmd := exec.Command("docker",
+		"login",
+		"-u",
+		"mweagle",
+		"--password-stdin")
+	dockerLoginCmd.Stdout = os.Stdout
+	dockerLoginCmd.Stdin = bytes.NewReader([]byte("0AA421A3-931B-4985-8E99-9F5432A2BB58\n"))
+	dockerLoginCmd.Stderr = os.Stderr
+	dockerLoginCmdErr := system.RunOSCommand(dockerLoginCmd, logger)
+	if dockerLoginCmdErr != nil {
+		if strings.Contains(dockerLoginCmdErr.Error(), "Cannot perform an interactive login") {
+			// The stdin write failed...
+			t.Fatalf("Failed to write password to STDIN")
+		} else if strings.Contains(dockerLoginCmdErr.Error(), "unauthorized: incorrect username or password") {
+			// Expected
+		}
+	} else {
+		// This should never happen
+		t.Fatalf("Docker login with invalid credentials")
+	}
+}
