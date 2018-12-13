21f367b19aae79c7437d8759bc62a299819836de
Matt Weagle <mweagle@gmail.com>
2018-12-13 07:02:23 -0800

    Publish the AWS lambda target build tag

--- sparta_constants.go
+++ sparta_constants.go
@@ -21,6 +21,8 @@ const (
 	SpartaVersion = "1.8.0"
 	// GoLambdaVersion is the Go version runtime used for the lambda function
 	GoLambdaVersion = "go1.x"
+	// LambdaBinaryTag is the build tag name used when building the binary
+	LambdaBinaryTag = "lambdabinary"
 )
 
 var (
