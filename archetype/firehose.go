544980aa755031f871279e120e317a73bef8d29e
Matt Weagle <mweagle@gmail.com>
2019-09-11 21:15:34 -0700

    Initial commit of Kinesis Firehose XFormer

---
+++ archetype/firehose.go
@@ -0,0 +1,237 @@
+package archetype
+
+import (
+	"archive/zip"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/Masterminds/sprig"
+	awsEvents "github.com/aws/aws-lambda-go/events"
+	awsLambdaEvents "github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws/session"
+	sparta "github.com/mweagle/Sparta"
+	"github.com/mweagle/Sparta/archetype/xformer"
+	gocf "github.com/mweagle/go-cloudformation"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func dropError() error {
+	return errors.New("KinesisFirehoseDrop")
+}
+
+var (
+	xformError error
+)
+
+// TemplateFileName is the name of the file in the ZIP archive
+const TemplateFileName = "xform.template"
+const xformResourcePrefix = "firehosexform_"
+const envVarKinesisFirehoseTransformName = "SPARTA_KINESIS_FIREHOSE_TRANSFORM"
+
+// KinesisFirehoseReactor represents a lambda function that responds to Dynamo  messages
+type KinesisFirehoseReactor interface {
+	// OnKinesisFirehoseRecord when an Kinesis reocrd occurs.
+	OnKinesisFirehoseRecord(ctx context.Context,
+		record *awsEvents.KinesisFirehoseEventRecord) (*awsEvents.KinesisFirehoseResponseRecord, error)
+}
+
+// KinesisFirehoseReactorFunc is a free function that adapts a KinesisFirehoseReactor
+// compliant signature into a function that exposes an OnEvent
+// function
+type KinesisFirehoseReactorFunc func(ctx context.Context,
+	kinesisRecord *awsEvents.KinesisFirehoseEventRecord) (*awsEvents.KinesisFirehoseResponseRecord, error)
+
+// OnKinesisFirehoseRecord satisfies the KinesisFirehoseReactor interface
+func (reactorFunc KinesisFirehoseReactorFunc) OnKinesisFirehoseRecord(ctx context.Context,
+	kinesisRecord *awsEvents.KinesisFirehoseEventRecord) (*awsEvents.KinesisFirehoseResponseRecord, error) {
+	return reactorFunc(ctx, kinesisRecord)
+}
+
+// ReactorName provides the name of the reactor func
+func (reactorFunc KinesisFirehoseReactorFunc) ReactorName() string {
+	return runtime.FuncForPC(reflect.ValueOf(reactorFunc).Pointer()).Name()
+}
+
+// NewKinesisFirehoseLambdaTransformer returns a new firehose proocessor that supports
+// transforming records.
+func NewKinesisFirehoseLambdaTransformer(reactor KinesisFirehoseReactor,
+	timeout time.Duration,
+	hooks *sparta.WorkflowHooks) (*sparta.LambdaAWSInfo, error) {
+
+	reactorLambda := func(ctx context.Context,
+		kinesisFirehoseEvent awsLambdaEvents.KinesisFirehoseEvent) (interface{}, error) {
+		// Apply the transform to each record and see
+		// what it says
+
+		response := &awsEvents.KinesisFirehoseResponse{
+			Records: make([]awsEvents.KinesisFirehoseResponseRecord,
+				len(kinesisFirehoseEvent.Records)),
+		}
+
+		for eachIndex, eachRecord := range kinesisFirehoseEvent.Records {
+			responseRecord, responseRecordErr := reactor.OnKinesisFirehoseRecord(ctx, &eachRecord)
+			if responseRecordErr != nil {
+				return nil, errors.Wrapf(responseRecordErr, "Failed to transform record")
+			}
+			if responseRecord == nil {
+				responseRecord = &awsEvents.KinesisFirehoseResponseRecord{
+					RecordID: eachRecord.RecordID,
+					Result:   awsEvents.KinesisFirehoseTransformedStateDropped,
+					Data:     eachRecord.Data,
+				}
+			}
+			response.Records[eachIndex] = *responseRecord
+		}
+		return response, nil
+	}
+
+	lambdaFn, lambdaFnErr := sparta.NewAWSLambda(reactorName(reactor),
+		reactorLambda,
+		sparta.IAMRoleDefinition{})
+	return lambdaFn, lambdaFnErr
+}
+
+// NewKinesisFirehoseTransformer returns a new firehose proocessor that supports
+// transforming records.
+func NewKinesisFirehoseTransformer(xformFilePath string,
+	timeout time.Duration,
+	hooks *sparta.WorkflowHooks) (*sparta.LambdaAWSInfo, error) {
+
+	baseName := filepath.Base(xformFilePath)
+	archiveEntryName := sparta.CloudFormationResourceName(xformResourcePrefix, xformFilePath)
+	lambdaName := fmt.Sprintf("Firehose%s", baseName)
+
+	// Return a lambda function that applies the XForm transformation
+	reactorLambda := func(ctx context.Context,
+		kinesisEvent awsEvents.KinesisFirehoseEvent) (*awsEvents.KinesisFirehoseResponse, error) {
+		return lambdaXForm(ctx, kinesisEvent)
+	}
+	lambdaFn, lambdaFnErr := sparta.NewAWSLambda(lambdaName,
+		reactorLambda,
+		sparta.IAMRoleDefinition{})
+	if lambdaFnErr != nil {
+		return nil, errors.Wrapf(lambdaFnErr, "attempting to create Kinesis Firehose reactor")
+	}
+
+	// Borrow the resource name creator to get a name for the archive
+	lambdaFn.Options.Environment[envVarKinesisFirehoseTransformName] = gocf.String(archiveEntryName)
+	lambdaFn.Options.Timeout = (timeout.Milliseconds() / 1000)
+
+	// Create the decorator that adds the file to the ZIP archive using
+	// the transform name...
+	archiveDecorator := func(context map[string]interface{},
+		serviceName string,
+		zipWriter *zip.Writer,
+		awsSession *session.Session,
+		noop bool,
+		logger *logrus.Logger) error {
+		fileInfo, fileInfoErr := os.Stat(xformFilePath)
+		if fileInfoErr != nil {
+			return errors.Wrapf(fileInfoErr, "Failed to get fileInfo for Kinesis Firehose transform")
+		}
+		fileReader, fileReaderErr := os.Open(xformFilePath)
+		if fileReaderErr != nil {
+			return errors.Wrapf(fileReaderErr, "Failed to open Kinesis Firehose transform file")
+		}
+		defer fileReader.Close()
+
+		fileHeader, fileHeaderErr := zip.FileInfoHeader(fileInfo)
+		if fileHeaderErr != nil {
+			return errors.Wrapf(fileHeaderErr, "Failed to detect ZIP header for Kinesis Firehose transform")
+		}
+
+		fileHeader.Name = archiveEntryName
+		fileHeader.Method = zip.Deflate
+
+		// Copy it...
+		writer, writerErr := zipWriter.CreateHeader(fileHeader)
+		if writerErr != nil {
+			return errors.Wrapf(fileHeaderErr, "Failed to create ZIP header for Kinesis Firehose transform")
+		}
+		_, copyErr := io.Copy(writer, fileReader)
+		return copyErr
+	}
+	// Done...
+	hooks.Archives = append(hooks.Archives, sparta.ArchiveHookFunc(archiveDecorator))
+	return lambdaFn, nil
+}
+
+// ApplyTransformToKinesisFirehoseEvent is the generic transformation function that applies
+// a template.Template transformation to each
+func ApplyTransformToKinesisFirehoseEvent(ctx context.Context,
+	templateBytes []byte,
+	kinesisEvent awsEvents.KinesisFirehoseEvent) (*awsEvents.KinesisFirehoseResponse, error) {
+
+	funcMap := sprig.TxtFuncMap()
+	funcMap["KinesisFirehoseDrop"] = interface{}(func() (string, error) {
+		return "", dropError()
+	})
+
+	// Setup the function map that knows how to do the JMESPath
+	// given the map...
+	transform, transformErr := template.
+		New("xformer").
+		Funcs(funcMap).
+		Parse(string(templateBytes))
+	if transformErr != nil {
+		return nil, errors.Wrapf(transformErr, "Attempting to create template")
+	}
+
+	response := &awsEvents.KinesisFirehoseResponse{
+		Records: make([]awsEvents.KinesisFirehoseResponseRecord, len(kinesisEvent.Records)),
+	}
+	headerInfo := &xformer.KinesisEventHeaderInfo{
+		InvocationID: kinesisEvent.InvocationID,
+
+		DeliveryStreamArn:      kinesisEvent.DeliveryStreamArn,
+		SourceKinesisStreamArn: kinesisEvent.SourceKinesisStreamArn,
+		Region:                 kinesisEvent.Region,
+	}
+
+	for eachIndex, eachRecord := range kinesisEvent.Records {
+		xformedRecord := awsEvents.KinesisFirehoseResponseRecord{
+			RecordID: eachRecord.RecordID,
+			Result:   awsEvents.KinesisFirehoseTransformedStateDropped,
+			Data:     eachRecord.Data,
+		}
+		xform, xformErr := xformer.NewKinesisFirehoseEventXFormer(headerInfo, &eachRecord)
+		if xformErr == nil {
+			dataMap := map[string]interface{}{
+				"Record": xform,
+			}
+			var outputBuffer bytes.Buffer
+			templateErr := transform.Execute(&outputBuffer, dataMap)
+			if templateErr != nil {
+				// Is the fail value "KinesisFirehoseDrop" ?
+				if !strings.Contains(templateErr.Error(), dropError().Error()) {
+					xformedRecord.Result = awsEvents.KinesisFirehoseTransformedStateProcessingFailed
+				}
+			} else if xform.Error() != nil {
+				xformedRecord.Result = awsEvents.KinesisFirehoseTransformedStateProcessingFailed
+			} else {
+				// logData := map[string]interface{}{
+				// 	"input":  eachRecord.Data,
+				// 	"output": outputBuffer.Bytes(),
+				// }
+				// logBytes, _ := json.MarshalIndent(logData, "", " ")
+				// fmt.Printf("%s", string(logBytes))
+
+				xformedRecord.Data = outputBuffer.Bytes()
+				xformedRecord.Result = awsEvents.KinesisFirehoseTransformedStateOk
+			}
+		}
+		// Save it...
+		response.Records[eachIndex] = xformedRecord
+	}
+	return response, nil
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.12
 require (
 	cloud.google.com/go v0.44.3 // indirect
 	github.com/AlecAivazis/survey/v2 v2.0.4
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver v1.4.2 // indirect
+	github.com/Masterminds/sprig v2.20.0+incompatible
 	github.com/Netflix/go-expect v0.0.0-20190729225929-0e00d9168667 // indirect
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/aws/aws-lambda-go v1.13.0
@@ -30,12 +33,16 @@ require (
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/google/pprof v0.0.0-20190723021845-34ac40c74b70
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.10.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.3 // indirect
 	github.com/hinshun/vt10x v0.0.0-20180809195222-d55458df857c // indirect
 	github.com/hokaccha/go-prettyjson v0.0.0-20190818114111-108c894c2c0e
+	github.com/huandu/xstrings v1.2.0 // indirect
 	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af
 	github.com/kisielk/errcheck v1.2.0 // indirect
 	github.com/klauspost/compress v1.8.1 // indirect
 	github.com/klauspost/cpuid v1.2.1 // indirect
@@ -47,8 +54,6 @@ require (
 	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
 	github.com/mholt/archiver v3.1.1+incompatible
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mweagle/go-cloudcondenser v0.0.0-20180209031108-e1ad78f4b780
 	github.com/mweagle/go-cloudformation v0.0.0-20190824022255-0b695fc6095a
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
@@ -95,5 +100,6 @@ require (
 	google.golang.org/grpc v1.23.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.29.1
-	honnef.co/go/tools v0.0.1-2019.2.2 // indirect
+	gopkg.in/ini.v1 v1.46.0 // indirect
+	honnef.co/go/tools v0.0.0-20190622161425-0d05180ad8c0 // indirect
 )
