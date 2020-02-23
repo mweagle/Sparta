2a889d84d7a5362e41c3d6c819b99944012dc799
Matt Weagle <mweagle@gmail.com>
2020-02-22 18:07:38 -0800

    Update hugo-theme-learn theme
    Ref: https://github.com/matcornic/hugo-theme-learn/commit/41c6cc522f13ac324e99e8bf84f2b67d88510ceb

--- docs_source/themes/hugo-theme-learn/static/js/hugo-learn.js
+++ docs_source/themes/hugo-theme-learn/static/js/hugo-learn.js
@@ -21,8 +21,13 @@ var images = $("div#body-inner img").not(".inline");
 // Wrap image inside a featherlight (to get a full size view in a popup)
 images.wrap(function(){
   var image =$(this);
-  if (!image.parent("a").length) {
-    return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+  var o = getUrlParameter(image[0].src);
+  var f = o['featherlight'];
+  // IF featherlight is false, do not use feather light
+  if (f != 'false') {
+    if (!image.parent("a").length) {
+      return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+    }
   }
 });
 
