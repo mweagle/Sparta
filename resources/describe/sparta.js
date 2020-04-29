a5a0b1eee0766878e21d196a222e015fa7cb3923
Matt Weagle <mweagle@gmail.com>
2020-04-28 22:16:33 -0700

    Update styles

--- resources/describe/sparta.js
+++ resources/describe/sparta.js
@@ -1,88 +1,85 @@
-var SERVICE_NAME = "TBD";
-var cytoscapeView = null;
+var SERVICE_NAME = 'TBD'
+var cytoscapeView = null
 
-function showView(newElementID) {
-  $("#view-container").children().hide();
-  $("#navBarItems").children().removeClass("active");
+function showView (newElementID) {
+  $('#view-container').children().hide()
+  $('#navBarItems').children().removeClass('active')
   // Set the tab active, the view active
-  viewID = "#" + newElementID + "-view";
-  tabID = "#" + newElementID + "-tab";
-  $(viewID).show();
-  $(tabID).addClass("active");
+  viewID = '#' + newElementID + '-view'
+  tabID = '#' + newElementID + '-tab'
+  $(viewID).show()
+  $(tabID).addClass('active')
 }
 
 $(document).ready(function () {
-  var cloudformationTemplate = null;
+  var cloudformationTemplate = null
   try {
-    cloudformationTemplate = JSON.parse(CLOUDFORMATION_TEMPLATE_RAW);
+    cloudformationTemplate = JSON.parse(CLOUDFORMATION_TEMPLATE_RAW)
   } catch (e) {
-    console.log("Failed to parse template: " + e.toString());
+    console.log('Failed to parse template: ' + e.toString())
     cloudformationTemplate = {
-      ERROR: e.toString(),
-    };
+      ERROR: e.toString()
+    }
   }
-  var jsonString = JSON.stringify(cloudformationTemplate, null, 4);
-  $("#rawTemplateContent").text(jsonString);
-  hljs.initHighlightingOnLoad();
-  $("pre code").each(function (i, block) {
-    hljs.highlightBlock(block);
-  });
+  var jsonString = JSON.stringify(cloudformationTemplate, null, 4)
+  $('#rawTemplateContent').text(jsonString)
+  hljs.initHighlightingOnLoad()
+  $('pre code').each(function (i, block) {
+    hljs.highlightBlock(block)
+  })
 
   try {
     // Show the cytoscape view
     cytoscapeView = window.cytoscapeView = cytoscape({
-      container: $("#cytoscapeDIVTarget"),
+      container: $('#cytoscapeDIVTarget'),
       elements: CYTOSCAPE_DATA,
       style: [{
-          selector: "node",
-          style: {
-            shape: "round-rectangle",
-            content: "data(label)",
-            "background-image": "data(image)",
-            "min-width": "196px",
-            "min-heigh": "196px",
-            "background-width": "196px",
-            "background-height": "196px",
-            "background-fit": "cover",
-            "background-opacity": "0",
-          },
-        },
+        selector: 'node',
+        style: {
+          shape: 'round-rectangle',
+          content: 'data(label)',
+          'background-image': 'data(image)',
+          'background-width': '100%',
+          'background-height': '100%',
+          'background-opacity': '0'
+        }
+      },
         {
-          selector: "edge",
+          selector: 'edge',
           style: {
-            content: "data(label)",
+            content: 'data(label)',
             width: 5,
-            "curve-style": "taxi",
-            "mid-target-arrow-shape": "triangle",
-          },
-        },
+            'curve-style': 'taxi',
+            'mid-target-arrow-shape': 'triangle'
+          }
+        }
       ],
       layout: {
-        name: "breadthfirst",
-      },
-    });
+        name: 'breadthfirst'
+      }
+    })
   } catch (err) {
-    console.log("Failed to initialize topology view: " + err);
+    console.log('Failed to initialize topology view: ' + err)
   }
   var layoutSelectorIDs = [
-    "#layout-breadthfirst",
-    "#layout-dagre",
-    "#layout-cose",
-    "#layout-grid",
-    "#layout-circle",
-    "#layout-concentric",
-  ];
+    '#layout-breadthfirst',
+    '#layout-dagre',
+    '#layout-cose',
+    '#layout-grid',
+    '#layout-circle',
+    '#layout-concentric'
+  ]
   layoutSelectorIDs.forEach(function (eachElement) {
     $(eachElement).click(function (event) {
-      event.preventDefault();
-      var layoutType = eachElement.split("-").pop();
-      console.log("Layout type: " + layoutType);
+      event.preventDefault()
+      var layoutType = eachElement.split('-').pop()
+      console.log('Layout type: ' + layoutType)
       cytoscapeView
         .makeLayout({
-          name: layoutType,
+          name: layoutType
         })
-        .run();
-    });
-  });
-  showView("lambda");
-});
+        .run()
+    })
+  })
+  showView('lambda')
+})
