9a0eb22da5f12d2b57211fa87a5926b1949815b5
Matt Weagle <mweagle@gmail.com>
2020-11-16 17:47:00 -0800

    Remove deleted files

--- provision_custom_resources.go
+++
@@ -1,9 +0,0 @@
-package sparta
-
-import "fmt"
-
-func customResourceDescription(serviceName string, targetType string) string {
-	return fmt.Sprintf("%s: CloudFormation CustomResource to configure %s",
-		serviceName,
-		targetType)
-}
