725c7f32c76c54368c989112c434e23f6c486529
Matt Weagle <mweagle@gmail.com>
2021-01-16 22:10:08 -0800

    Add package doc

---
+++ cloudtest/doc.go
@@ -0,0 +1,6 @@
+/**Package cloudtest exposes a BDD style interface to supporting
+Lambda function integration tests that depend on either event event
+triggers or side effects.
+**/
+
+package cloudtest
