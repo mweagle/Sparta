e129c4c561562f3e7e0e477d17ad48d232433769
Matt Weagle <mweagle@gmail.com>
2021-10-21 20:50:30 -0700

    Add AWS API Task State
    Ref: https://aws.amazon.com/blogs/aws/now-aws-step-functions-supports-200-aws-services-to-enable-easier-workflow-automation/

---
+++ aws/step/aws_api_test.go
@@ -0,0 +1,24 @@
+package step
+
+import (
+	"testing"
+
+	gof "github.com/awslabs/goformation/v5/cloudformation"
+	sparta "github.com/mweagle/Sparta"
+)
+
+func TestAWSAPIStepFunction(t *testing.T) {
+	awsGetMetadataState := NewAWSSDKState("getMetadata",
+		"s3",
+		"headObject",
+		"",
+		map[string]interface{}{
+			"Bucket": "weagle",
+			"Key.$":  "$.SampleDataInputKey",
+		})
+	startMachine := NewStateMachine("AWSAPIMachine", awsGetMetadataState).WithRoleArn(gof.GetAtt("StepMachineRole", "Arn"))
+
+	testStepProvision(t,
+		[]*sparta.LambdaAWSInfo{},
+		startMachine)
+}
